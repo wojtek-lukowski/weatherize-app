e878cfe63c37c87921ae31a182a3194ecef792d7
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-03-30 15:26:34 +0200

    basic data displayed in HTML

--- script/script.js
+++ script/script.js
@@ -13,6 +13,18 @@ async function getWeather(lat, lng) {
     const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}`;
     const data = await (await fetch(api)).json();
     console.log(data);
+    console.log(data.name);
+    console.log(data.sys.country);
+    console.log(data.main.temp - 273.15);
+
+    const tempData = (data.main.temp - 273.15).toFixed(1);
+
+    const location = document.getElementById('location');
+    const temp = document.getElementById('temperature');
+    const sky = document.getElementById('sky');
+    location.innerHTML = `${data.name}, ${data.sys.country}`;
+    temp.innerHTML = `${tempData} C°`;
+    sky.innerHTML = data.weather[0].main;
 
   } catch(err) {
    console.log(err)
